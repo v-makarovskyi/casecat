11c014cbc513d96f560f2ab3de7d1bc66687755b
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-07-26 14:10:57 +0300

    added Navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,38 +9,56 @@ export default async function Navbar() {
   return (
     <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
-        <div>
-          <Link href="/">
-            case <span>CAT</span>
+        <div className="h-14 flex items-center justify-between border-b border-zinc-200">
+          <Link href="/" className="flex z-40 font-semibold">
+            case <span className="text-green-600">CAT</span>
           </Link>
-          <div>
+          <div className="h-full flex flex-row items-center space-x-4">
             {!user ? (
               <>
-                <Link href="/api/auth/logout" className="">
+                <Link href="/api/auth/logout" className={buttonVariants({
+                    size: 'sm',
+                    variant: 'ghost'
+                })}>
                   Sign out
                 </Link>
                 {isAdmin ? (
-                  <Link href="/dashboard" className="">
-                    Dashboard 📎
+                  <Link href="/dashboard" className={buttonVariants({
+                    size:'sm',
+                    variant:'ghost'
+                  })}>
+                    Dashboard ✨
                   </Link>
                 ) : null}
-                <Link href="/configure/upload" className="">
-                  Create case <ArrowRight />
+                <Link href="/configure/upload" className={buttonVariants({
+                    size:'sm',
+                    className:'hidden sm:flex items-center gap-1'
+                })}>
+                  Create case <ArrowRight className="w-5 h-5 ml-1.5"/>
                 </Link>
               </>
             ) : (
               <>
-                <Link href="/api/auth/register" className="">
+                <Link href="/api/auth/register" className={buttonVariants({
+                    size:'sm',
+                    variant:'ghost'
+                })}>
                   Sign up
                 </Link>
-                <Link href="/api/auth/login" className="">
+                <Link href="/api/auth/login" className={buttonVariants({
+                    size: 'sm',
+                    variant:'ghost'
+                })}>
                   Login
                 </Link>
 
-                <div />
+                <div className="hidden sm:block h-8 w-px bg-zinc-200"/>
 
-                <Link href="/configure/upload">
-                  Create case <ArrowRight />
+                <Link href="/configure/upload" className={buttonVariants({
+                    size:'sm',
+                    className: 'hidden sm:flex items-center gap-1'
+                })}>
+                  Create case <ArrowRight className="h-5 w-5 ml-1.5"/>
                 </Link>
               </>
             )}
