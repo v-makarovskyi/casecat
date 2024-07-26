a015e6daeba0401743e2ab268f5ad634fd5ee892
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-07-26 14:11:24 +0300

    buttonVariants cva library

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,49 +1,42 @@
 import React from "react";
 import { cn } from "@/lib/utils";
-import { cva, VariantProps } from 'class-variance-authority'
-import { Slot } from '@radix-ui/react-slot'
+import { cva, VariantProps } from "class-variance-authority";
+import { Slot } from "@radix-ui/react-slot";
 
-const buttonVariants = ''
-
-//const Button = ''
-
-const button = cva("button", {
+const buttonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50",
+  {
     variants: {
-      intent: {
-        primary: [
-          "bg-blue-500",
-          "text-white",
-          "border-transparent",
-          "hover:bg-blue-600",
-        ],
-        secondary: [
-          "bg-white",
-          "text-gray-800",
-          "border-gray-400",
-          "hover:bg-gray-100",
-        ],
+      variant: {
+        default: "",
+        destructive: "",
+        outline: "",
+        secondary: "",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "",
       },
       size: {
-        small: ["text-sm", "py-1", "px-2"],
-        medium: ["text-base", "py-2", "px-4"],
+        default: "",
+        sm: "h-8 text-xs rounded-md px-3",
+        lg: "",
+        icon: "",
       },
     },
-    compoundVariants: [{ intent: "primary", size: "medium", class: "uppercase" }],
     defaultVariants: {
-      intent: "primary",
-      size: "medium",
+      variant: "default",
+      size: "default",
     },
-  });
-  
-  export interface ButtonProps
-    extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-      VariantProps<typeof button> {}
-  
-  export const Button: React.FC<ButtonProps> = ({
-    className,
-    intent,
-    size,
-    ...props
-  }) => <button className={button({ intent, size, className })} {...props} />;
+  }
+);
 
-export { buttonVariants, Button }
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+  isLoading?: boolean;
+  loadingText?: string;
+}
+
+const Button = "";
+
+export { buttonVariants, Button };
