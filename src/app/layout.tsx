40bbce8ba4c51a5875d32a877f74bf626647d030
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-07-27 15:51:20 +0300

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,26 @@
 import React from "react";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
-import { Recursive } from 'next/font/google'
+import { Recursive } from "next/font/google";
+import Providers from "@/components/providers";
 import "./globals.css";
 
-const recursive = Recursive({ subsets: ['latin'] })
+const recursive = Recursive({ subsets: ["latin"] });
 
-export default function RootLayout({children}:Readonly<{children: React.ReactNode}>) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body className={`${recursive.className}`}>
         <Navbar />
-        <main>
-          <div>
-            {children}
+        <main className="flex flex-col grainy-light min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">
+            <Providers>{children}</Providers>
           </div>
           <Footer />
         </main>
       </body>
     </html>
-  )
+  );
 }
