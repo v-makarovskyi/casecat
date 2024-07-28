a0ee105337626b51e6bd0b12f9580f74322cbd2e
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-07-28 15:14:10 +0300

    added component

---
+++ src/components/icons.tsx
@@ -0,0 +1,24 @@
+import { LucideProps } from "lucide-react";
+
+export const Icons = {
+  underline: (props: LucideProps) => {
+    return (
+      <svg {...props} viewBox="0 0 687 155">
+        <g
+          fill="none"
+          fillRule="evenodd"
+          stroke="currentColor"
+          stroke-width="7"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path
+            d="M20 98c27-13.3333333 54-20 81-20 40.5 0 40.5 20 81 20s40.626917-20 81-20 40.123083 20 80.5 20 40.5-20 81-20 40.5 20 81 20 40.626917-20 81-20c26.915389 0 53.748722 6.6666667 80.5 20"
+            opacity=".3"
+          ></path>
+          <path d="M20 118c27-13.3333333 54-20 81-20 40.5 0 40.5 20 81 20s40.626917-20 81-20 40.123083 20 80.5 20 40.5-20 81-20 40.5 20 81 20 40.626917-20 81-20c26.915389 0 53.748722 6.6666667 80.5 20" opacity='.8'></path>
+        </g>
+      </svg>
+    );
+  },
+};
